4525e4dcabbc6b375f928248883d6e6f25f9f22d
Bill Hilbert <billmakes@protonmail.com>
2022-12-09 10:07:47 -0500

    tailwind

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import AngularComponent from './app.component';
 import NgVueDirective from './directives/ng-vue-directive';
 import '../style.css';
+import '../../dist/output.css';
 
 angular
   .module('app', [])
